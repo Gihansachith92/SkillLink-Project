7b62fbae9d5e33f29b82e2497032ed2c0d3849b1
Gihan_Sachith <gihansachith92@gmail.com>
2026-02-16 23:37:14 +0530

    Set up project with Maven, Spring Boot, PostgreSQL, and Lombok integrations. Added basic project structure, `pom.xml`, and `mvnw` scripts.

---
+++ skill-link-backend/src/main/java/com/gihan/skilllinkbackend/SkillLinkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gihan.skilllinkbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillLinkBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkillLinkBackendApplication.class, args);
+    }
+
+}
---
+++ skill-link-backend/src/test/java/com/gihan/skilllinkbackend/SkillLinkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gihan.skilllinkbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillLinkBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
