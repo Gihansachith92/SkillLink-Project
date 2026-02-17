4afc1f350da553f92c8fbad39782d005fc5d23ba
Gihan_Sachith <gihansachith92@gmail.com>
2026-02-17 22:21:06 +0530

    create User entity

---
+++ skill-link-backend/src/main/java/com/gihan/skilllinkbackend/model/User.java
@@ -0,0 +1,28 @@
+package com.gihan.skilllinkbackend.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String university;
+
+}
