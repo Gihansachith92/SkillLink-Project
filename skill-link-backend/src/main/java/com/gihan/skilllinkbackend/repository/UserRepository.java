aae9cacdabf3177ff12970dbf5058f10d80b3d9d
Gihan_Sachith <gihansachith92@gmail.com>
2026-02-17 22:35:10 +0530

    create User repository

---
+++ skill-link-backend/src/main/java/com/gihan/skilllinkbackend/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.gihan.skilllinkbackend.repository;
+
+import com.gihan.skilllinkbackend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+}
