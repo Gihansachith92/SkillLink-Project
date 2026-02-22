3f26f1b8767a25513c9990438d50b298226fd321
Gihan_Sachith <gihansachith92@gmail.com>
2026-02-22 21:05:00 +0530

    feat: enhance login component with standalone setup and improved authentication handling

--- skill-link-frontend/src/app/pages/login/login.ts
+++ skill-link-frontend/src/app/pages/login/login.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { Api } from '../../services/api';
 
 @Component({
   selector: 'app-login',
-  imports: [],
-  templateUrl: './login.html',
-  styleUrl: './login.css',
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterLink],
+  templateUrl: './login.html'
 })
 export class Login {
+
+  credentials = {
+    email: '',
+    password: ''
+  };
+
+  message = '';
+  isSuccess = false;
+
+  constructor(private apiService: Api, private cdr: ChangeDetectorRef){}
+
+  onSubmit(){
+
+    this.message = 'Authenticating....';
+
+    this.apiService.login(this.credentials).subscribe({
+
+      next: (response: any) => {
+        this.message = response.message;
+        this.isSuccess = true;
+        this.credentials = {email: '', password: ''};
+
+        this.cdr.detectChanges();
+      },
+      error: (err) => {
+        this.message = err.error?.error || 'Login failed. Please try again.';
+        this.isSuccess = false;
+
+        this.cdr.detectChanges();
+      }
+
+    });
+
+  }
 
 }
