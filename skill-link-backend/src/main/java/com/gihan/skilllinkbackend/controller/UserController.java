60e3d478c049a2e8b04abb5c537f4b7d21fdbc42
Gihan_Sachith <gihansachith92@gmail.com>
2026-02-24 23:54:50 +0530

    feat: allow user profile updates via new UserController and expand security config to permit user-related API requests

---
+++ skill-link-backend/src/main/java/com/gihan/skilllinkbackend/controller/UserController.java
@@ -0,0 +1,35 @@
+package com.gihan.skilllinkbackend.controller;
+
+import com.gihan.skilllinkbackend.dto.UpdateProfileRequest;
+import com.gihan.skilllinkbackend.model.User;
+import com.gihan.skilllinkbackend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/users")
+@CrossOrigin(origins = "http://localhost:4200")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PutMapping("/{id}/profile")
+    public ResponseEntity<?> updateProfile(@PathVariable  Long id, @RequestBody UpdateProfileRequest request){
+        try{
+            User updatedUser = userService.updateUserProfile(id,request);
+
+            return ResponseEntity.ok(Map.of(
+                    "message", "Profile updated successfully!",
+                    "user", updatedUser
+            ));
+        }catch (RuntimeException e){
+            return ResponseEntity.badRequest().body(Map.of("error", e.getMessage()));
+        }
+
+    }
+
+}
