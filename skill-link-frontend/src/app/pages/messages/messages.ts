1d60a4e0e87fd085996b9c3ddb3afab979fefa3a
Gihan_Sachith <gihansachith92@gmail.com>
2026-04-02 16:38:23 +0530

    feat: enhance Messages component with chat functionality and user validation

--- skill-link-frontend/src/app/pages/messages/messages.ts
+++ skill-link-frontend/src/app/pages/messages/messages.ts
@@ -1,11 +1,50 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectorRef, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Client } from '@stomp/stompjs';
+import { Api } from '../../services/api';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-messages',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './messages.html',
   styleUrl: './messages.css',
 })
-export class Messages {
+export class Messages implements OnInit, OnDestroy{
+
+  @ViewChild('chatScroll') private chatScrollContainer!: ElementRef;
+
+  currentUser: any = null;
+  chatPartnerId: number | null = null;
+  chatPartner: any = null;
+
+  // chat variables
+  stompClient!: Client;
+  messages: any[] = [];
+  newMessage: string = '';
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private apiService: Api,
+    private http: HttpClient,
+    private cdr: ChangeDetectorRef
+  ) {}
+
+  ngOnInit() {
+    this.currentUser = this.apiService.getUser();
+    if(!this.currentUser) {
+      this.router.navigate(['/login']);
+      return;
+    }
+
+    this.route.paramMap.subscribe(params => {
+      const idParam = params.get('id');
+      if(idParam)
+    });
+  }
 
 }
