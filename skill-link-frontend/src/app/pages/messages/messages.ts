0832b78aa420f85559fd122a61d5d26afb1064a5
Gihan_Sachith <gihansachith92@gmail.com>
2026-04-02 18:11:50 +0530

    feat: implement chat partner profile loading and chat history retrieval with WebSocket connection

--- skill-link-frontend/src/app/pages/messages/messages.ts
+++ skill-link-frontend/src/app/pages/messages/messages.ts
@@ -5,6 +5,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Client } from '@stomp/stompjs';
 import { Api } from '../../services/api';
 import { HttpClient } from '@angular/common/http';
+import SockJS from 'sockjs-client';
 
 @Component({
   selector: 'app-messages',
@@ -43,8 +44,57 @@ export class Messages implements OnInit, OnDestroy{
 
     this.route.paramMap.subscribe(params => {
       const idParam = params.get('id');
-      if(idParam)
+      if(idParam){
+        this.chatPartnerId = +idParam;
+        this.loadChatPartnerProfile();
+        this.loadChatHistory();
+        this.connectWebSocket();
+      }
     });
+  }
+
+  ngOnDestroy() {
+    if(this.stompClient){
+      this.stompClient.deactivate();
+    }
+  }
+
+  // --- 1. REST API: Fetch History & Profile ---
+
+  loadChatPartnerProfile() {
+    if(!this.chatPartnerId) return;
+    this.apiService.getUserProfile(this.chatPartnerId).subscribe({
+      next: (profile) => {
+        this.chatPartner = profile;
+        this.cdr.detectChanges();
+      },
+      error: (err) => console.error('Failed to load chat partner', err)
+    });
+  }
+
+  loadChatHistory() {
+    if(!this.chatPartnerId) return;
+    this.http.get<any[]>(`http://localhost:8080/api/messages/${this.currentUser.id}/${this.chatPartnerId}`)
+    .subscribe({
+      next: (history) => {
+        this.messages = history;
+        this.cdr.detectChanges();
+        this.scrollToBottom();
+      },
+      error: (err) => console.error('Failed to load history', err)
+    });
+  }
+
+  // --- 2. WEBSOCKET: Real-Time Connection ---
+
+  connectWebSocket() {
+    this.stompClient = new Client({
+      webSocketFactory: () => new SockJS('http://localhost:8080/ws'),
+      reconnectDelay: 5000,
+      debug: (str) => console.log(str)
+    });
+
+    this.stompClient
   }
 
 }
