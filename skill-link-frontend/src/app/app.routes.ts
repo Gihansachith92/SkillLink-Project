6006f1a410ec0cc54cefd375e7e58fb81e6ede36
Gihan_Sachith <gihansachith92@gmail.com>
2026-04-03 20:06:51 +0530

    feat: add routing for messages page and update index.html for global window object

--- skill-link-frontend/src/app/app.routes.ts
+++ skill-link-frontend/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Register } from './pages/register/register';
 import { Login } from './pages/login/login';
 import { Dashboard } from './pages/dashboard/dashboard';
 import { Network } from './pages/network/network';
+import { Messages } from './pages/messages/messages';
 
 export const routes: Routes = [
   {path: 'register', component: Register},
   {path: 'login', component: Login},
   {path: 'dashboard', component: Dashboard},
   { path: 'network', component: Network },
+  { path: 'messages/:id', component: Messages },
   {path: '', redirectTo: 'login', pathMatch: 'full'}
 ];
